1ce0058dcc18099940ad7ea397544d4f4a7fe78e
Maharshee roy <m.roy@optonline.net>
2018-11-06 22:17:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size > 0
   puts"The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-end
 else
   puts "The line is currently empty."
+end
 end
 
 def take_a_number(katz_deli, name)
